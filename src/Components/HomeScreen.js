29be2e26a50b13bdfef1094ca0da6169d21532d1
eelopez515 <eelopez15@gmail.com>
2021-02-08 20:44:11 -0500

    Rows Render rows with movies in accordance to their genre and added styling

--- src/Components/HomeScreen.js
+++ src/Components/HomeScreen.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Nav from './Nav'
 import Banner from './Banner'
+import Row from './Row'
 import '../Style/HomeScreen.css'
+import requests from '../API/Requests'
 
 function HomeScreen() {
     return (
@@ -11,7 +13,39 @@ function HomeScreen() {
             {/* Banner */}
             <Banner />
             {/* Rows */}
-
+            <Row
+            title='NETFLIX ORIGINALS'
+            fetchUrl={requests.fetchNetflixOriginals}
+            isLargeRow
+            />
+            <Row
+            title='Trending Now'
+            fetchUrl={requests.fetchTrending}
+            />
+            <Row
+            title='Top Rated'
+            fetchUrl={requests.fetchTopRated}
+            />
+            <Row
+            title='Action Movies'
+            fetchUrl={requests.fetchActionMovies}
+            />
+            <Row
+            title='Comedy Movies'
+            fetchUrl={requests.fetchComedyMovies}
+            />
+            <Row
+            title='Horror Movies'
+            fetchUrl={requests.fetchHorrorMovies}
+            />
+            <Row
+            title='Romance Movies'
+            fetchUrl={requests.fetchRomanceMovies}
+            />
+            <Row
+            title='Documentaries'
+            fetchUrl={requests.fetchDocumentaries}
+            />
         </div>
     )
 }
